3635799a831016b3875181dfa67e42802ca1592a
osama rabie <osamaguc@gmail.com>
2019-12-29 10:03:48 +0200

    1- Example project added

---
+++ ExampleOsamaRabieFirstFrameWork/ExampleOsamaRabieFirstFrameWork/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ExampleOsamaRabieFirstFrameWork
+//
+//  Created by Osama Rabie on 29/12/2019.
+//  Copyright © 2019 Osama Rabie. All rights reserved.
+//
+
+import UIKit
+import OsamaRabieFirstFrameWork
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(RandomGenerator.string())
+    }
+
+
+}
+
---
+++ ExampleOsamaRabieFirstFrameWork/Pods/Target Support Files/OsamaRabieFirstFrameWork/OsamaRabieFirstFrameWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OsamaRabieFirstFrameWork : NSObject
+@end
+@implementation PodsDummy_OsamaRabieFirstFrameWork
+@end
