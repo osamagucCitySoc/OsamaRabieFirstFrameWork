89e3aba74e51aa98a4ce9566cf5e306333a4df1c
osama rabie <osamaguc@gmail.com>
2019-12-30 10:33:55 +0200

    1.2.0 version sync from Xcode

--- OsamaRabieFirstFrameWork/OsamaRabieFirstFrameWork/KnobView.swift
+++ OsamaRabieFirstFrameWork/OsamaRabieFirstFrameWork/KnobView.swift
@@ -27,7 +27,7 @@ public class KnobView: UIView {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        // latest 1.0.17
+        // latest 1.2.0
         super.init(coder: aDecoder)
         commonInit()
     }
